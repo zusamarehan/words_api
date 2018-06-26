8ce00cacb691d4b1262aaf1bd72490c108e84fbf
zRehan <zrehan286@gmail.com>
2018-06-27 00:57:49 +0530

    chaned host

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'wordgameapi.000webhostapp.com',
+  host     : 'localhost',
   user     : 'id5874260_root',
   password : 'usamarehab_!@#$',
   database : 'id5874260_words_api'
