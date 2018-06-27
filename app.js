bea821b6558d76d72e3dff9e2ae4240a406f31e2
zRehan <zrehan286@gmail.com>
2018-06-27 22:06:04 +0530

    changed the database using akram id

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 const express = require('express');
 const app = express();
 
-// var mysql      = require('mysql');
-// var connection = mysql.createConnection({
-//   host     : 'wordgameapi.000webhostapp.com',
-//   user     : 'id5874260_root',
-//   password : 'usamarehab_!@#$',
-//   database : 'id5874260_words_api'
-// });
-// connection.connect();
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'sql12.freemysqlhosting.net',
+  user     : 'sql12244912',
+  password : '2Drmp4WWwX',
+  database : 'sql12244912'
+});
+connection.connect();
 
 
-// app.get('/getwords', function(request, response){
-//     connection.query('select * from words_api', function(error, results){
-//         if ( error ){
-//             response.status(400).send('Error in database operation');
-//         } else {
-//             response.send(results);
-//         }
-//     });
-// });
+app.get('/getwords', function(request, response){
+    connection.query('select * from words_api', function(error, results){
+        if ( error ){
+            response.status(400).send('Error in database operation');
+        } else {
+            response.send(results);
+        }
+    });
+});
 
-// app.get('/getwords/:id', function(request, response){
+app.get('/getwords/:id', function(request, response){
      
-//      connection.query('select * from words_api where id >= '+request.params.id+' LIMIT 8', function(error, results){
-//         if ( error ){
-//             response.status(400).send('Error in database operation');
-//         } else {
-//             response.send(results);
-//         }
-//     });
-// });
+     connection.query('select * from words_api where id >= '+request.params.id+' LIMIT 8', function(error, results){
+        if ( error ){
+            response.status(400).send('Error in database operation');
+        } else {
+            response.send(results);
+        }
+    });
+});
 
 app.get('/', (req, res) => res.send('Cool'));
 
