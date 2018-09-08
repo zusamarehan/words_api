4c2dd43c669bf47e74cf6b1f49618adc3b3f0a34
Usama Rehan <zrehan286@gmail.com>
2018-09-08 19:57:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ var connection = mysql.createConnection({
   host     : process.env.DB_HOST,
   user     : process.env.DB_USER,
   password : process.env.DB_PASS,
-  database : process.env.DB_USER,
-  connectTimeout : 5000,
+  database : process.env.DB_USER
 });
 connection.connect();
 
